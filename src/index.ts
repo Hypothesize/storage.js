e01afcbac40c88ab85499454c0e030fe30975788
Arthur <arthur.plouet@gmail.com>
2020-04-23 17:39:43 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-import { generate } from "./repository"
-import { Repository as generateApiRepository } from "./api-repository"
-import { Repository as generatePgRepository } from "./pg-repository"
-
-export const Greeter = (name: string) => `Hello ${name}`
-export const repository = generate
-export const ApiRepository = generateApiRepository
-export const PgRepository = generatePgRepository
+import * as Repository from "./repository"
+export default Repository
