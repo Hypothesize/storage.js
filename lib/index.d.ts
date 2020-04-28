e9d96d9b360876c5c1316883b28a65ca30ccc082
Arthur <arthur.plouet@gmail.com>
2020-04-28 18:30:16 +0800

    Update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 export * from './repository';
 export { Repository as ApiRepository } from './api-repository';
-export { Repository as PgRepository } from './pg-repository';
+export declare let PgRepository: any;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export * from './repository'
 export { Repository as ApiRepository } from './api-repository'
-export { Repository as PgRepository } from './pg-repository'
+
+export let PgRepository;
+
+if (typeof module !== 'undefined' && module.exports) {
+	PgRepository = require('./pg-repository')
+} else {
+	PgRepository = {}
+}
